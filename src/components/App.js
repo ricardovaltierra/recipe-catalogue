6d22f9a4a745c07340cdb6bbde6f050bc0406929
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-09-30 11:50:59 -0500

    added Detail component with route

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import DishList from '../containers/DishList';
+import Detail from '../components/Detail';
 
 const App = () => (
   <div>
     <Router>
       <Switch>
         <Route exact path="/" component={DishList} />
+        <Route path="/detail" component={Detail} />
       </Switch>
     </Router>
   </div>
---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Detail = () => (
+  <div>
+    <h2>ID:</h2>
+    <h3>Dish category: </h3>
+    <h1>Dish name:</h1>
+  </div>
+);
+
+export default Detail;
