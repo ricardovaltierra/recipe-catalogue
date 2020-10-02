da4c286a638af2db730dd04bd5e03cca049e8bb4
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-02 16:03:00 -0500

    styling ingredients tab

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,79 +5,126 @@ const getRandomID = () => {
   return Math.floor(Math.random() * Math.floor(5000));
 };
 
-const Detail = props => {
-  try {
-    const dish = props.location.dish;
-    const Preview = () => (
-      <div className='detail-prev'>
-        <div className='detail-main'>
-          <h2 className='detail-title'>{dish.label}</h2>
-          <p className='detail-calories'>{Number(dish.calories.toFixed(0))} cal</p>
-        </div>
-        <p className='detail-source'>Posted by <strong>{dish.source}</strong></p>
-      </div>
-    );
-    const Ingredients = (
-      <div className='detail-ingredients'>
-        <h2>{dish.label}</h2>
-        <ul>
-          {
-            dish.healthLabels.map(label => {
-            return  <li key={ label.substring(0,2).concat(getRandomID()) }>{ label }</li> 
-            })
-          }
-        </ul>
-        <ul>
-          {
-            dish.ingredients.map(ingredient => {
-            return  <li key={ ingredient.text.substring(0,2).concat(getRandomID()) }>{ ingredient.text }</li> 
-            })
-          }
-        </ul>
-      </div>
-    );
-    const Facts = (
-      <div className='detail-facts'>
-        
-      </div>
-    );
-    console.log(dish);
-    return (
-      <div id={dish.uri} className='detail-wrapper'>
-        <div className='nav-top'>
-          <Link to={{pathname: '/'}} className='return-link'>
-          <i className='nav-icon'>V</i>
-          </Link>
-          <p className="nav-title">maindish</p>
-          <i className='dropicon nav-icon'>Q</i>
-        </div>
-        <span style={{ backgroundImage: 'url(' + dish.image + ')' }} className='detail-img'>
-          <div className='detail-tab'>
-            <p className='tab-item'>Preview</p>
-            <p className='tab-item'>Ingredients</p>
-            <p className='tab-item'>Facts</p>
-          </div>
-          <div className='details'>
-            <Preview />
-          </div>
-        </span>
-      </div>
-    );
-  } catch (error) {
-    return (
-      <div className='detail-wrapper'>
-        <div className='nav-top'>
-          <Link to={{pathname: '/'}}>
-            <i className='nav-icon'>V</i>
-          </Link>
-          <p className="nav-title">maindish</p>
-          <i className='dropicon nav-icon'>Q</i>
-        </div>
-        <h1 className='detail-error'>Page not found :(</h1>
-      </div>
-    );
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tabOption: 0,
+      dish: null,
+      preview: null,
+      ingredients: null,
+      facts: null,
+      currentTab: null
+    };
+
+    this.handleTabChange = this.handleTabChange.bind(this);
   }
-  
+
+  handleTabChange(e) {
+    let currentTab = null;
+    switch (e.target.innerHTML) {
+      case 'Preview':
+        currentTab = this.state.preview;
+        break;
+      case 'Ingredients':
+        currentTab = this.state.ingredients;
+        break;
+      case 'Facts':
+        currentTab = this.state.facts;
+        break;
+      default:
+        currentTab = this.state.preview;
+        break;
+    }
+    this.setState({
+      currentTab: currentTab
+    });
+  }
+
+  componentWillMount(){
+    const dish = this.props.location.dish;
+      const Preview = () => (
+        <div className='detail-prev'>
+          <div className='detail-main'>
+            <h2 className='detail-title'>{dish.label}</h2>
+            <p className='detail-calories'>{Number(dish.calories.toFixed(0))} cal</p>
+          </div>
+          <p className='detail-source'>Posted by <strong>{dish.source}</strong></p>
+        </div>
+      );
+      const Ingredients = () => (
+        <div className='detail-ingredients'>
+          <h2 className='detail-title'>{dish.label}</h2>
+          <div className='detail-ul-hl'>
+            {
+              dish.healthLabels.map(label => {
+              return  <p className='detail-li-hl' key={ label.substring(0,2).concat(getRandomID()) }>{ label }</p> 
+              })
+            }
+          </div>
+          <ul className='detail-ul-ig'>
+            {
+              dish.ingredients.map(ingredient => {
+              return  <li className='detail-li-ig' key={ ingredient.text.substring(0,2).concat(getRandomID()) }><input type='checkbox' class='check-ig'/><label for='checkbox'><span>{ ingredient.text }</span></label></li> 
+              })
+            }
+          </ul>
+        </div>
+      );
+      const Facts = () => (
+        <div className='detail-facts'>
+          <h1>Example</h1>
+        </div>
+      );
+
+    this.setState({
+      dish: dish,
+      preview: <Preview />,
+      ingredients: <Ingredients />,
+      facts: <Facts />,
+      currentTab: <Preview />
+    });
+  }
+
+  render() {
+    try {
+      return (
+        <div className='detail-wrapper'>
+          <div className='nav-top'>
+            <Link to={{pathname: '/'}} className='return-link'>
+            <i className='nav-icon'>V</i>
+            </Link>
+            <p className="nav-title">maindish</p>
+            <i className='dropicon nav-icon'>Q</i>
+          </div>
+          <span style={{ backgroundImage: 'url(' + this.state.dish.image + ')' }} className='detail-img'>
+            <div className='detail-tab'>
+              <p className='tab-item' onClick={this.handleTabChange}>Preview</p>
+              <p className='tab-item' onClick={this.handleTabChange}>Ingredients</p>
+              <p className='tab-item' onClick={this.handleTabChange}>Facts</p>
+            </div>
+            <div className='details' id='details'>
+              {this.state.currentTab}
+            </div>
+          </span>
+        </div>
+      );
+    } catch (error) {
+      return (
+        <div className='detail-wrapper'>
+          <div className='nav-top'>
+            <Link to={{pathname: '/'}}>
+              <i className='nav-icon'>V</i>
+            </Link>
+            <p className="nav-title">maindish</p>
+            <i className='dropicon nav-icon'>Q</i>
+          </div>
+          <h1 className='detail-error'>Page not found :(</h1>
+        </div>
+      );
+    }
+  }
 };
 
 export default Detail;
