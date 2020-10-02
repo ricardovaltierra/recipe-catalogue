5389f6b3ac9c0bc83d5c9f12e4b7fd94d94b1855
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-02 06:49:26 -0500

    Detail component styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import DishList from '../containers/DishList';
 import Detail from '../components/Detail';
 
-const App = () =>(
-      <div className='app-wrapper'>
-        <div className='nav-top'>
-          <i className='nav-icon'>O</i>
-          <p className="nav-title">sweet</p>
-          <i className='dropicon nav-icon'>Q</i>
-        </div>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={DishList} />
-            <Route exact path="/:dish_name" component={Detail} />
-          </Switch>
-        </Router>
-      </div>
-    );
+const App = () =>(        
+  <Router>
+    <Switch>
+      <Route exact path="/" component={DishList} />
+      <Route exact path="/:dish_name" component={Detail} />
+    </Switch>
+  </Router>
+);
 
 export default App;
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,38 +6,78 @@ const getRandomID = () => {
 };
 
 const Detail = props => {
-  const dish = props.location.dish;
-  return (
-      <div id={dish.uri}>
-        <Link to={{pathname: '/'}}>
-          <p>Recipe menu</p>
-        </Link>
-        <h1>{dish.label}</h1>
-        <h3>Category: {dish.healthLabels[0] || 'No category'}</h3>
-        <img src={dish.image} size='50px' alt={dish.label}/>
-        <h2>{Number(dish.calories.toFixed(0))} calories</h2>
-        <div>
-          <h2>Health Labels</h2>
-          <ul>
-            {
-              dish.healthLabels.map(label => {
-              return  <li key={ label.substring(0,2).concat(getRandomID()) }>{ label }</li> 
-              })
-            }
-          </ul>
+  try {
+    const dish = props.location.dish;
+    const Preview = () => (
+      <div className='detail-prev'>
+        <div className='detail-main'>
+          <h2 className='detail-title'>{dish.label}</h2>
+          <p className='detail-calories'>{Number(dish.calories.toFixed(0))} cal</p>
         </div>
-        <div>
-          <h2>Ingredients</h2>
-          <ul>
-            {
-              dish.ingredients.map(ingredient => {
-              return  <li key={ ingredient.text.substring(0,2).concat(getRandomID()) }>{ ingredient.text }</li> 
-              })
-            }
-          </ul>
-        </div>
+        <p className='detail-source'>Posted by <strong>{dish.source}</strong></p>
       </div>
     );
+    const Ingredients = (
+      <div className='detail-ingredients'>
+        <h2>{dish.label}</h2>
+        <ul>
+          {
+            dish.healthLabels.map(label => {
+            return  <li key={ label.substring(0,2).concat(getRandomID()) }>{ label }</li> 
+            })
+          }
+        </ul>
+        <ul>
+          {
+            dish.ingredients.map(ingredient => {
+            return  <li key={ ingredient.text.substring(0,2).concat(getRandomID()) }>{ ingredient.text }</li> 
+            })
+          }
+        </ul>
+      </div>
+    );
+    const Facts = (
+      <div className='detail-facts'>
+        
+      </div>
+    );
+    console.log(dish);
+    return (
+      <div id={dish.uri} className='detail-wrapper'>
+        <div className='nav-top'>
+          <Link to={{pathname: '/'}} className='return-link'>
+          <i className='nav-icon'>V</i>
+          </Link>
+          <p className="nav-title">maindish</p>
+          <i className='dropicon nav-icon'>Q</i>
+        </div>
+        <span style={{ backgroundImage: 'url(' + dish.image + ')' }} className='detail-img'>
+          <div className='detail-tab'>
+            <p className='tab-item'>Preview</p>
+            <p className='tab-item'>Ingredients</p>
+            <p className='tab-item'>Facts</p>
+          </div>
+          <div className='details'>
+            <Preview />
+          </div>
+        </span>
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className='detail-wrapper'>
+        <div className='nav-top'>
+          <Link to={{pathname: '/'}}>
+            <i className='nav-icon'>V</i>
+          </Link>
+          <p className="nav-title">maindish</p>
+          <i className='dropicon nav-icon'>Q</i>
+        </div>
+        <h1 className='detail-error'>Page not found :(</h1>
+      </div>
+    );
+  }
+  
 };
 
 export default Detail;
