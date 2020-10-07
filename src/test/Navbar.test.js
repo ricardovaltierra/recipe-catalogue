0a3f198317f1ac9f98c8bdb90188789612a16c35
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-10-07 14:55:49 -0500

    removed test files

--- src/test/Navbar.test.js
+++ src/test/Navbar.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Navbar } from '../components/Navbar';
 
 test('renders the correct content', () => {
